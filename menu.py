446956b4f7c8c8136841bce896c1e18f405fab08
JoshJovanW <69958793+JoshJovanW@users.noreply.github.com>
2021-02-04 22:04:57 +0700

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,20 @@
+def Menu():
+    print("Welcome to Starbucks at The Rocks \n")
+
+    print("131 - 135 George Street, The Rocks,")
+    print("Sydney, NSW 2000\n")
+
+    print("Find us on George Street at the edgeof First Fleet Park & next door to ")
+    print("Sydney's oldest pub, Fortune of War.\n")
+
+    print("Sweét, Creämy and réfréshing\n")
+
+    print("As of today there are 4 drinks available:\n ")
+
+    print("- The Seasonal Soy Lattes" + "      " + "- The Signature Java Chip Frappucinos\n")
+    
+    print("- The Special Almond Ice Coffees" + "  " + "- The Exquisite Caramel Macchiatos\n")
+
+    print("\n")
+
+    print("To find out more about the drinks read the information below: \n")
