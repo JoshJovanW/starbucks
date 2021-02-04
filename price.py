5e30ab36206bdf2329dd10955c23e05d59a9e817
JoshJovanW <69958793+JoshJovanW@users.noreply.github.com>
2021-02-04 22:04:30 +0700

    Create price.py

---
+++ price.py
@@ -0,0 +1,27 @@
+def price_Menu():
+    print("The prices are listed below: \n") 
+   
+    #Latte prices
+    print("Lattes"  +  "          "  + "Tall  " +    "    "  +  "$4.25\n")
+    print("Lattes"  +  "          "  + "Grande" +  "    "  + "$4.95\n")
+    print("Lattes"  +  "          "  + "Venti " +   "    " + "$5.25\n")
+
+    #Frappucino prices
+    print("Frappucino"  +  "      "  + "Tall  "  +   "    "  +  "$3.45\n")
+    print("Frappucino"  +  "      "  + "Grande"  + "    "  +  "$4.15\n")
+    print("Frappucino"  +  "      "  + "Venti "  +  "    "  +  "$4.45\n")
+
+    #Ice Coffee prices
+    print("Ice Coffee"  +  "      "  + "Tall  " +    "    "  + "$3.55\n")
+    print("Ice Coffee"  +  "      "  + "Grande" +  "    "  + "$4.25\n")
+    print("Ice Coffee"  +  "      "  + "Venti " +   "    "  +  "$4.55\n")
+ 
+    #Macchiato prices
+    print("Macchiatos"  +  "      "  + "Tall  " +    "    "  + "$4.99\n")
+    print("Macchiatos"  +  "      "  + "Grande" +  "    " + "$5.69\n")
+    print("Macchiatos"  +  "      "  + "Venti " +   "    " + "$5.99\n") 
+
+    #TOPPING PRICES
+    print("MAKE IT YOURS?" + " " + "add jelly(+2) * only for ice coffee " + " / " + "add 2 shots(+0.5) *only for latte " + " / " + "whip cream (+2)*only for java chip frappucino " + " / " + "cloud foam (+1) *only for macchiato " + "less sugar (free) ")
+    print("\n")
+    print("* Don't forget to contact your barista if you have any allergies whatsoever.\n")
